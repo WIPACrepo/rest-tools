66016820552c6e3452a2d45413772ca53fc4c1e2
David Schultz <davids24@gmail.com>
2019-02-21 13:32:14 -0600

    add common code for daemonizing a server

--- rest_tools/server/__init__.py
+++ rest_tools/server/__init__.py
@@ -1,3 +1,4 @@
 from .server import RestServer
 from .handler import RestHandlerSetup, RestHandler, authenticated, catch_error, role_authorization
 from .auth import Auth
+from .daemon import Daemon
