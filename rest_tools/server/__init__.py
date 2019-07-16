0ca3f445b971c709d7223c05084dd75800b0a7ec
David Schultz <davids24@gmail.com>
2019-07-16 12:43:04 -0500

    add handy short import

--- rest_tools/server/__init__.py
+++ rest_tools/server/__init__.py
@@ -1,4 +1,4 @@
 from .server import RestServer
-from .handler import RestHandlerSetup, RestHandler, authenticated, catch_error, role_authorization
+from .handler import RestHandlerSetup, RestHandler, authenticated, catch_error, role_authorization, scope_role_auth
 from .auth import Auth
 from .daemon import Daemon
