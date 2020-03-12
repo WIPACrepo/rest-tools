cdfeacdaafd546ed96821ce57d646146ed75a9e4
Ric Evans <eevans@icecube.wisc.edu>
2020-03-12 15:41:53 -0500

    add import for from_environment

--- rest_tools/server/__init__.py
+++ rest_tools/server/__init__.py
@@ -2,3 +2,4 @@ from .server import RestServer
 from .handler import RestHandlerSetup, RestHandler, authenticated, catch_error, role_authorization, scope_role_auth
 from .auth import Auth
 from .daemon import Daemon
+from .config import from_environment
