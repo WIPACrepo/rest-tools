9c1730fc8a92c3d44517f560d71e15535c7510e1
Ric Evans <emejqz@gmail.com>
2021-02-18 14:06:28 -0600

    stub out tests

---
+++ tests/server/arghandler_test.py
@@ -0,0 +1,47 @@
+"""Test server/arghandler.py."""
+
+
+def test_qualify_argument() -> None:
+    """Test `_qualify_argument()`."""
+    pass
+
+
+def test_type_check() -> None:
+    """Test `_type_check()`."""
+    pass
+
+
+def test_get_argument_no_body_00() -> None:
+    """Test `request.arguments`/`RequestHandler.get_argument()` arguments.
+
+    No tests for body-parsing.
+    """
+    pass
+
+
+def test_get_argument_no_body_01() -> None:
+    """Test `request.arguments`/`RequestHandler.get_argument()` arguments.
+
+    No tests for body-parsing.
+    """
+    pass
+
+
+def test_get_json_body_argument_00() -> None:
+    """Test `request.body` JSON arguments."""
+    pass
+
+
+def test_get_json_body_argument_01() -> None:
+    """Test `request.body` JSON arguments."""
+    pass
+
+
+def test_get_argument_args_and_body_00() -> None:
+    """Test `get_argument()`."""
+    pass
+
+
+def test_get_argument_args_and_body_01() -> None:
+    """Test `get_argument()`."""
+    pass
