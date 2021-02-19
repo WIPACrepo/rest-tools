1c58e6f7ceb33aade4fdd99649b5d4987caaa9b3
Ric Evans <emejqz@gmail.com>
2021-02-18 18:29:04 -0600

    test get_argument() (w/ json body parsing)

--- tests/server/arghandler_test.py
+++ tests/server/arghandler_test.py
@@ -55,26 +55,33 @@ def test_01_qualify_argument_errors() -> None:
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(int, [], "")
     assert "(ValueError)" in str(e.value)
+    assert "400" in str(e.value)
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(float, [], "")
     assert "(ValueError)" in str(e.value)
+    assert "400" in str(e.value)
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(float, [], "123abc")
     assert "(ValueError)" in str(e.value)
+    assert "400" in str(e.value)
 
     # Test Choices:
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(None, [23], "23")
     assert "(ValueError)" in str(e.value) and "not in options" in str(e.value)
+    assert "400" in str(e.value)
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(str, ["STRING"], "string")
     assert "(ValueError)" in str(e.value) and "not in options" in str(e.value)
+    assert "400" in str(e.value)
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(int, [0, 1, 2], "3")
     assert "(ValueError)" in str(e.value) and "not in options" in str(e.value)
+    assert "400" in str(e.value)
     with pytest.raises(tornado.web.HTTPError) as e:
         ArgumentHandler._qualify_argument(list, [["a", "b", "c", "d"]], "abc")
     assert "(ValueError)" in str(e.value) and "not in options" in str(e.value)
+    assert "400" in str(e.value)
 
 
 def test_10_type_check() -> None:
@@ -104,20 +111,58 @@ def test_10_type_check() -> None:
                 ArgumentHandler._type_check(o_type, val)
 
 
-def test_20_get_argument_no_body() -> None:
+@patch("rest_tools.server.arghandler._get_json_body")
+@patch("tornado.web.RequestHandler")
+def test_20_get_argument_no_body(twrh: Mock, gjb: Mock) -> None:
     """Test `request.arguments`/`RequestHandler.get_argument()` arguments.
 
     No tests for body-parsing.
     """
-    pass
+    args = {
+        "foo": ("-10", int),
+        "bar": ("True", bool),
+        "bat": ("2.5", float),
+        "baz": ("Toyota Corolla", str),
+        "boo": ("False", bool),
+    }
+
+    for arg, (val, type_) in args.items():
+        # pylint: disable=E1102
+        print(arg)
+        print(val)
+        print(type_)
+        # w/o typing
+        gjb.return_value = {}
+        twrh.get_argument.return_value = val
+        ret = ArgumentHandler.get_argument(twrh, arg, None, False, None, [])
+        assert ret == val
+        # w/ typing
+        gjb.return_value = {}
+        twrh.get_argument.return_value = val
+        ret = ArgumentHandler.get_argument(twrh, arg, None, False, type_, [])
+        assert ret == type_(val) or (val == "False" and ret is False and type_ == bool)
+
+    # NOTE - `default` non-error use-cases solely on RequestHandler.get_argument(), so no tests
+    # NOTE - `strip` use-cases depend solely on RequestHandler.get_argument(), so no tests
+    # NOTE - `choices` use-cases are tested in `_qualify_argument` tests
 
 
-def test_21_get_argument_no_body() -> None:
+@patch("rest_tools.server.arghandler._get_json_body")
+@patch("tornado.web.RequestHandler")
+def test_21_get_argument_no_body_errors(twrh: Mock, gjb: Mock) -> None:
     """Test `request.arguments`/`RequestHandler.get_argument()` arguments.
 
     No tests for body-parsing.
     """
-    pass
+    # Missing Required Argument
+    with pytest.raises(tornado.web.HTTPError) as e:
+        gjb.return_value = {}
+        twrh.get_argument.side_effect = tornado.web.MissingArgumentError("Reqd")
+        ArgumentHandler.get_argument(twrh, "Reqd", NO_DEFAULT, False, None, [])
+    assert "Reqd" in str(e.value)
+    assert "400" in str(e.value)
+
+    # NOTE - `type_` and `choices` are tested in `_qualify_argument` tests
 
 
 @patch("rest_tools.server.arghandler._get_json_body")
@@ -137,7 +182,7 @@ def test_30_get_json_body_argument(gjb: Mock) -> None:
         ret = ArgumentHandler.get_json_body_argument(ANY, arg, "Terry", [])
         assert ret == "Terry"
 
-    # NOTE - `choices` use-cases are tested `_qualify_argument` tests
+    # NOTE - `choices` use-cases are tested in `_qualify_argument` tests
 
 
 @patch("rest_tools.server.arghandler._get_json_body")
@@ -151,12 +196,14 @@ def test_31_get_json_body_argument_errors(gjb: Mock) -> None:
         ArgumentHandler.get_json_body_argument(ANY, "Reqd", NO_DEFAULT, [])
     assert "Reqd" in str(e.value)
 
+    # NOTE - `choices` use-cases are tested in `_qualify_argument` tests
+
 
 def test_40_get_argument_args_and_body() -> None:
     """Test `get_argument()`."""
     pass
 
-    # NOTE - `type_` and `choices` are tested `_qualify_argument` tests
+    # NOTE - `type_` and `choices` are tested in `_qualify_argument` tests
 
 
 def test_41_get_argument_args_and_body_errors() -> None:
