155af5a566e24ffa6e35ba12f1cf8c32d6b5878b
Ric Evans <emejqz@gmail.com>
2021-02-24 15:58:38 -0600

    mypy-fix: from_environment()

--- rest_tools/server/config.py
+++ rest_tools/server/config.py
@@ -4,14 +4,25 @@
 import os
 from typing import cast, Dict, Mapping, Optional, Sequence, Union
 
-OptionalDict = Mapping[str, Optional[str]]
-KeySpec = Union[str, Sequence[str], OptionalDict]
 RetVal = Union[str, int, float, bool]
+OptionalDict = Mapping[str, Optional[RetVal]]
+KeySpec = Union[str, Sequence[str], OptionalDict]
+
+
+def _typecast(source: str, type_: type) -> RetVal:
+    if type_ == bool:
+        # NOTE - could use distutils.strtobool (but wouldn't be backwards-compatible)
+        return source.lower() in ("true", "t", "1", "yes", "y")
+    elif type_ == int:
+        return int(source)
+    elif type_ == float:
+        return float(source)
+    else:
+        return source
 
 
 def from_environment(keys: KeySpec) -> Dict[str, RetVal]:
-    """
-    Obtain configuration values from the OS environment.
+    """Obtain configuration values from the OS environment.
 
     keys - Specify the configuration values to obtain.
 
@@ -56,18 +67,17 @@ def from_environment(keys: KeySpec) -> Dict[str, RetVal]:
         keys = dict.fromkeys(keys, None)
     elif not isinstance(keys, dict):
         raise TypeError("keys: Expected string, list or dict")
+
     config = keys.copy()
+
     for key in config:
         if key in os.environ:
-            val = os.environ[key]
             if config[key] is not None:
-                if isinstance(config[key], bool):
-                    val = val.lower() in ('true', 't', '1', 'yes', 'y')
-                elif isinstance(config[key], int):
-                    val = int(val)
-                elif isinstance(config[key], float):
-                    val = float(val)
-            config[key] = val
+                config[key] = _typecast(os.environ[key], type(config[key]))
+            else:
+                config[key] = os.environ[key]
+
         elif config[key] is None:
             raise OSError(f"Missing environment variable '{key}'")
-    return cast(Dict[str, str], config)
+
+    return cast(Dict[str, RetVal], config)
