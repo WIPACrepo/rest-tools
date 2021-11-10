3285be73ecc47957270ccc985b3ee0977913fd99
Ric Evans <emejqz@gmail.com>
2021-11-10 17:12:03 -0600

    add `__all__` to all sub-packages

--- rest_tools/server/__init__.py
+++ rest_tools/server/__init__.py
@@ -1,5 +1,28 @@
-from .server import RestServer
-from .handler import RestHandlerSetup, RestHandler, authenticated, catch_error, role_authorization, scope_role_auth
+"""Sub-package __init__."""
+
 from .auth import Auth, OpenIDAuth
-from .daemon import Daemon
 from .config import from_environment
+from .daemon import Daemon
+from .handler import (
+    RestHandler,
+    RestHandlerSetup,
+    authenticated,
+    catch_error,
+    role_authorization,
+    scope_role_auth,
+)
+from .server import RestServer
+
+__all__ = [
+    "RestServer",
+    "RestHandlerSetup",
+    "RestHandler",
+    "authenticated",
+    "catch_error",
+    "role_authorization",
+    "scope_role_auth",
+    "Auth",
+    "OpenIDAuth",
+    "Daemon",
+    "from_environment",
+]
