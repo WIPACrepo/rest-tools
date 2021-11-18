476ee5ed9cff252a98206bb1e93a370d746677a7
Ric Evans <emejqz@gmail.com>
2021-11-18 16:00:57 -0600

    fix imports and rearrange tests

--- rest_tools/utils/__init__.py
+++ rest_tools/utils/__init__.py
@@ -1,5 +1,14 @@
 """Sub-package __init__."""
 
 from . import json_util
+from .auth import Auth, OpenIDAuth
+from .config import from_environment
+from .daemon import Daemon
 
-__all__ = ["json_util"]
+__all__ = [
+    "json_util",
+    "Auth",
+    "OpenIDAuth",
+    "Daemon",
+    "from_environment",
+]
