4d0f368e0c72f3a30da49c65fae42cb7556dbcda
David Schultz <davids24@gmail.com>
2023-03-08 12:33:15 -0600

    add example clients (#113)
    * add example clients
    
    * <bot> update requirements-dev.txt
    
    * <bot> update requirements-telemetry.txt
    
    * <bot> update requirements-tests.txt
    
    * <bot> update requirements.txt
    
    * fix flake8
    
    * clarify that we actually have functional RestClients, in case someone takes these examples as an application base
    
    * fix flake8
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ examples/get_client_credentials_token.py
@@ -0,0 +1,24 @@
+import argparse
+
+from rest_tools.client import ClientCredentialsAuth
+
+
+def get_token(address, client_id, client_secret):
+    rest_client = ClientCredentialsAuth('', token_url=address, client_id=client_id, client_secret=client_secret)
+    # could use `rest_client` as a `RestClient`, but we're only interested in the token
+    return rest_client.make_access_token()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Get an OAuth2 token via client credentials')
+    parser.add_argument('--address', default='https://keycloak.icecube.wisc.edu/auth/realms/IceCube', help='OAuth2 server address')
+    parser.add_argument('client_id', help='client id')
+    parser.add_argument('client_secret', help='client secret')
+
+    args = parser.parse_args()
+    kwargs = vars(args)
+    print('access token:', get_token(**kwargs))
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/get_device_credentials_token.py
@@ -0,0 +1,23 @@
+import argparse
+
+from rest_tools.client import SavedDeviceGrantAuth
+
+
+def get_token(address, client_id):
+    rest_client = SavedDeviceGrantAuth('', token_url=address, filename='device-refresh-token', client_id=client_id)
+    # could use `rest_client` as a `RestClient`, but we're only interested in the token
+    return rest_client._openid_token()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Get an OAuth2 token via client credentials')
+    parser.add_argument('--address', default='https://keycloak.icecube.wisc.edu/auth/realms/IceCube', help='OAuth2 server address')
+    parser.add_argument('client_id', help='client id')
+
+    args = parser.parse_args()
+    kwargs = vars(args)
+    print('access token:', get_token(**kwargs))
+
+
+if __name__ == '__main__':
+    main()
