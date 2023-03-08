7f1f7489efe2ad6f4e9ea3f26565fe8b3826571d
David Schultz <david.schultz@icecube.wisc.edu>
2023-03-08 11:02:52 -0600

    fix flake8

--- examples/get_client_credentials_token.py
+++ examples/get_client_credentials_token.py
@@ -7,6 +7,7 @@ def get_token(address, client_id, client_secret):
     cc = ClientCredentialsAuth('', token_url=address, client_id=client_id, client_secret=client_secret)
     return cc.make_access_token()
 
+
 def main():
     parser = argparse.ArgumentParser(description='Get an OAuth2 token via client credentials')
     parser.add_argument('--address', default='https://keycloak.icecube.wisc.edu/auth/realms/IceCube', help='OAuth2 server address')
@@ -16,6 +17,7 @@ def main():
     args = parser.parse_args()
     kwargs = vars(args)
     print('access token:', get_token(**kwargs))
+
 
 if __name__ == '__main__':
     main()
--- examples/get_device_credentials_token.py
+++ examples/get_device_credentials_token.py
@@ -7,6 +7,7 @@ def get_token(address, client_id):
     cc = SavedDeviceGrantAuth('', token_url=address, filename='device-refresh-token', client_id=client_id)
     return cc._openid_token()
 
+
 def main():
     parser = argparse.ArgumentParser(description='Get an OAuth2 token via client credentials')
     parser.add_argument('--address', default='https://keycloak.icecube.wisc.edu/auth/realms/IceCube', help='OAuth2 server address')
@@ -15,6 +16,7 @@ def main():
     args = parser.parse_args()
     kwargs = vars(args)
     print('access token:', get_token(**kwargs))
+
 
 if __name__ == '__main__':
     main()
