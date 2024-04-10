e7f872a82edcd769e3c5364f50bc4378eeb9dfcd
ric-evans <emejqz@gmail.com>
2024-04-10 15:26:53 -0500

    pt 2

--- rest_tools/client/utils.py
+++ rest_tools/client/utils.py
@@ -1,7 +1,7 @@
 """Utility functions for RestClient."""
 
 import logging
-from typing import Any, Optional
+from typing import Any, Optional, Dict
 
 import requests
 
@@ -25,7 +25,7 @@ def request_and_validate(
     openapi_spec: "openapi_core.OpenAPI",
     method: str,
     path: str,
-    args: Optional[dict[str, Any]] = None,
+    args: Optional[Dict[str, Any]] = None,
 ) -> Any:
     """Make request and validate the response against a given OpenAPI spec.
 
