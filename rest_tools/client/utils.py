a0ce13a4cc3af1fb0b64f60622f90d38a81d99f7
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2024-05-22 11:56:13 -1000

    Use openapi's `RequestsOpenAPIResponse` and Log on Error (#147)
    * Use `openapi_core_requests.RequestsOpenAPIResponse` and Log on Error
    
    * <bot> update dependencies*.log files(s)
    
    * take 2
    
    * better logging
    
    * stable logging
    
    * remove breakpoint
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- rest_tools/client/utils.py
+++ rest_tools/client/utils.py
@@ -15,6 +15,7 @@ LOGGER = logging.getLogger(__name__)
 try:
     import openapi_core
     from openapi_core.contrib import requests as openapi_core_requests
+    from openapi_core.exceptions import OpenAPIError
 except ImportError:
     pass  # if client code wants to use these features, then let the built-in errors raise
 
@@ -37,33 +38,18 @@ async def request_and_validate(
     # run request as async in case of other dependent, concurrent actions (ex: test suite runs server in same process)
     response = await asyncio.wrap_future(rc.session.request(method, url, **kwargs))  # type: ignore[var-annotated,arg-type]
 
-    # duck typing magic
-    class _DuckResponse(openapi_core.protocols.Response):
-        """AKA 'openapi_core_requests.RequestsOpenAPIResponse' but correct."""
-
-        @property
-        def data(self) -> Optional[bytes]:
-            return response.content
-
-        @property
-        def status_code(self) -> int:
-            return int(response.status_code)
-
-        @property
-        def content_type(self) -> str:
-            # application/json; charset=UTF-8  ->  application/json
-            # ex: openapi_core.validation.response.exceptions.DataValidationError: DataValidationError: Content for the following mimetype not found: application/json; charset=UTF-8. Valid mimetypes: ['application/json']
-            return str(response.headers.get("Content-Type", "")).split(";")[0]
-            # alternatively, look at how 'openapi_core_requests.RequestsOpenAPIRequest.mimetype' handles similarly
-
-        @property
-        def headers(self) -> dict:
-            return dict(response.headers)
-
-    openapi_spec.validate_response(
-        openapi_core_requests.RequestsOpenAPIRequest(response.request),
-        _DuckResponse(),
-    )
+    try:
+        openapi_spec.validate_response(
+            openapi_core_requests.RequestsOpenAPIRequest(response.request),
+            openapi_core_requests.RequestsOpenAPIResponse(response),
+        )
+    except OpenAPIError as e:
+        LOGGER.error(
+            f"OpenAPI response validator encountered an error: '{e}'; more info below."
+        )
+        LOGGER.info(f"request: {vars(response.request)}")
+        LOGGER.info(f"response: {vars(response)}")
+        raise
 
     out = rc._decode(response.content)
     response.raise_for_status()
